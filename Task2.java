d9942507da94283f8a46eeffc31b8e5a32d7b8bf
Uri_Klevcov <a-08-137.mail.ru>
2023-02-18 00:04:30 +0600

    Result

--- Task2.java
+++ Task2.java
@@ -6,9 +6,17 @@ import java.io.IOException;
 
 public class Task2 {
     public static void main(String[] args) {
-        CreateFile("HomeWork.txt");
-        WriteMessageToFile("Homework.txt", "Добрый день      Как дела    Все хорошо");
-        System.out.println(ReadFromFile("HomeWork.txt"));
+        String taskFileName = "HomeWork.txt";
+        String resultFileName = "Result.txt";
+        String text = "Добрый день      Как дела    Все хорошо";
+        
+        CreateFile(taskFileName);
+        WriteMessageToFile(taskFileName, text);
+        String textFromFile = ReadFromFile(taskFileName);
+        String[] words = ArrayWords(textFromFile);
+        Dotting(words);
+        String resultText = ArrayToString(words);
+        WriteMessageToFile(resultFileName, resultText);
     }
 
     static void CreateFile(String fileName) {
@@ -48,5 +56,22 @@ public class Task2 {
         return sb.toString();
     }
 
+    static String[] ArrayWords(String text) {
+        text = text.replaceAll("\\s+", " ");
+        return text.split(" ");
+    }   
     
+    static String ArrayToString(String[] array) {
+        return String.join(" ", array);
+    }
+
+    static void Dotting(String[] array) {
+        int len = array.length;
+        for (int i = 1; i < len; i++) {
+            if (Character.isUpperCase(array[i].charAt(0))) {
+                array[i-1] = array[i-1] + ".";
+            }
+        }
+        array[len - 1] = array[len - 1] + ".";
+    }
 }
